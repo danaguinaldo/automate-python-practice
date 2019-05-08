a8a1d343fb0de3f64a7f361d6bd9033a6294eb8f
Danny Aguinaldo <dan.r.aguinaldo@gmail.com>
2019-05-08 09:00:14 -0500

    Write to file

--- randomQuizGenerator.py
+++ randomQuizGenerator.py
@@ -52,3 +52,7 @@ for quizNum in range(35):
         quizFile.write('\n')
 
         #TODO: Write the answer key to a file.
+        answerKeyFile.write('%s. %s\n' % (questionNum + 1, 'ABCD'[answerOptions.index(correctAnswer)]))
+
+quizFile.close()
+answerKeyFile.close()
