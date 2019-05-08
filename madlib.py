43602a6229d19680291015408cc3feaf361cdaa5
Danny Aguinaldo <dan.r.aguinaldo@gmail.com>
2019-05-08 13:48:32 -0500

    Start madlib project

---
+++ madlib.py
@@ -0,0 +1,12 @@
+#! python3
+# madlib.py - Create a story with user input.
+
+import re, os
+
+#TODO: Prompt Input
+
+#TODO: Open file
+
+#TODO: Match ADJ, NOUN, VERB, ADVERB
+
+#TODO: Write to file
