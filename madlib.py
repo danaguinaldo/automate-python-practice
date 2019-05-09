15f47eb9be77fd9fb833971f54106916498eb279
Danny Aguinaldo <dan.r.aguinaldo@gmail.com>
2019-05-09 10:49:41 -0500

    Regex to search and replace for madlib

--- madlib.py
+++ madlib.py
@@ -3,10 +3,29 @@
 
 import re, os
 
-#TODO: Prompt Input
+madlibRegex = re.compile(r'(ADJECTIVE)|(NOUN)|(VERB)|(ADVERB)')
 
-#TODO: Open file
+#TODO: Read File
+madTextFile = open("./madlib/madlib.txt", 'r')
+madText = madTextFile.read()
+madTextFile.close()
 
 #TODO: Match ADJ, NOUN, VERB, ADVERB
+while re.search(madlibRegex, madText) != None:
+    if re.search(madlibRegex, madText).group() == 'ADJECTIVE':
+        adj = input("Enter an adjective: ")
+        madText = re.sub(madlibRegex, adj, madText, 1)
+    elif re.search(madlibRegex, madText).group() == 'NOUN':
+        noun = input("Enter a noun: ")
+        madText = re.sub(madlibRegex, noun, madText, 1)
+    elif re.search(madlibRegex, madText).group() == 'VERB':
+        verb = input("Enter a verb: ")
+        madText = re.sub(madlibRegex, verb, madText, 1)
+    elif re.search(madlibRegex, madText).group() == 'ADVERB':
+        adverb = input("Enter an adverb: ")
+        madText = re.sub(madlibRegex, adverb, madText, 1)
 
 #TODO: Write to file
+madTextFile = open("./madlib/madlib.txt", 'w')
+madTextFile.write(madText)
+madTextFile.close()
